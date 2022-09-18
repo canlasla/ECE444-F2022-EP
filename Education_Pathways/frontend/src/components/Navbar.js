75382c2da9ef232717e94b6359375c282626a92b
canlasla <lancecanlas25@gmail.com>
2022-09-17 22:03:54 -0400

    Added new tab to nav bar with corresponding page

--- Education_Pathways/frontend/src/components/Navbar.js
+++ Education_Pathways/frontend/src/components/Navbar.js
@@ -1,116 +1,139 @@
 import React, { Component } from 'react';
-import './css/navbar.css'
+import './css/navbar.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import logo from './img/logo.png'
-import { Navbar, Nav } from "react-bootstrap";
-import { BrowserRouter as Router, Route, Switch, Link, useLocation } from "react-router-dom";
+import logo from './img/logo.png';
+import { Navbar, Nav } from 'react-bootstrap';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Link,
+	useLocation,
+} from 'react-router-dom';
 // import LogIn from "./LogIn.jsx";
-import CourseDescriptionPage from "./CourseDescription";
+import CourseDescriptionPage from './CourseDescription';
 // import Wishlist from './Wishlist';
 // import SignUp from './SignUp'
-import SearchResultDisplay from './ResultDisplay'
+import SearchResultDisplay from './ResultDisplay';
 
-function CourseDescription (props) {
-  let query = useQuery();
-  return <CourseDescriptionPage code={query.get("code")} />;
+function CourseDescription(props) {
+	let query = useQuery();
+	return <CourseDescriptionPage code={query.get('code')} />;
 }
 
 function useQuery() {
-  const { search } = useLocation();
+	const { search } = useLocation();
 
-  return React.useMemo(() => new URLSearchParams(search), [search]);
+	return React.useMemo(() => new URLSearchParams(search), [search]);
 }
 
-
 export default class NavbarComp extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: localStorage.getItem('username'),
+			login: false,
+		};
+	}
 
-  constructor(props){
-    super(props)
-    this.state = {
-      username: localStorage.getItem('username'),
-      login: false
-    }
-  }
+	componentDidMount() {
+		if (localStorage.getItem('username') !== '') {
+			this.setState({ username: localStorage.getItem('username') });
+		}
+	}
 
-  componentDidMount() {
-    if (localStorage.getItem('username') !== "") {
-      this.setState({username: localStorage.getItem('username')})
-    }
-  }
+	logOut = () => {
+		localStorage.setItem('username', '');
+		this.setState({ username: '' });
+	};
 
-  logOut = () => {
-    localStorage.setItem('username', "");
-    this.setState({username: ""})
-  }
+	render() {
+		return (
+			<Router>
+				<div>
+					<Navbar bg='myBlue' variant='dark' sticky='top' expand='lg'>
+						<Navbar.Brand>
+							<img src={logo} alt='' />{' '}
+							<Nav.Link href='/' style={{ color: 'white', display: 'inline' }}>
+								Education Pathways
+							</Nav.Link>
+						</Navbar.Brand>
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <Navbar bg="myBlue" variant="dark" sticky="top" expand="lg">
-            <Navbar.Brand>
-              <img src={logo} alt="" />{" "}
-              <Nav.Link href="/" style={{ color: "white", display: "inline" }}>
-                Education Pathways
-              </Nav.Link>
-            </Navbar.Brand>
+						<Navbar.Toggle />
+						<Navbar.Collapse>
+							<Nav>
+								<Nav.Link as={Link} to='/about'>
+									About Us
+								</Nav.Link>
 
-            <Navbar.Toggle />
-            <Navbar.Collapse>
-              <Nav>
-                <Nav.Link as={Link} to="/about">
-                  About Us
-                </Nav.Link>
-
-                {/* <Nav.Link href="/search" style={{ color: "white", display: "inline" }}>
+								{/* <Nav.Link href="/search" style={{ color: "white", display: "inline" }}>
                   Search
                 </Nav.Link> */}
+							</Nav>
 
-                
-
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-        <div>
-          <Switch>
-          <Route path="/about">
-            <div className = "body_text">
-            <p>
-
-      Welcome to CARTE's in-development tool for course selection at UofT. Education Pathways allows for more intelligent course searching, by matching not just the terms you search, but ones relevant to them. The more terms you search for, the more relevant your results will be! Even try searching across disciplines for the courses that best cover each.
-
-Whatever year you are looking for, Education Pathways will also suggest courses in earlier years that will best help you to prepare. To get the most out of this, try searching for courses in a later year and see what is suggested for your current one.
-
-We are looking for feedback to improve Education Pathways and make it more useful for students. If you have ideas or suggestions, please <a href = "mailto:alex.olson@utoronto.ca">  email us! </a> <br></br>
-</p>
-<p> 
-  <b>Development Team: </b>
-</p>
-<p>Alexander Olson <a href="https://carte.utoronto.ca/"> (CARTE)</a> </p>
-<p>Student team from <a href="https://shuiblue.github.io/UofT-ECE444/">ECE444-Fall2021</a> : Janelle Cuevas, Jean Lin, Terry Luan, Cansin Varol, Nick Woo</p>
-
-
-      </div>
-              {/* <SearchResultDisplay /> */}
-            </Route>
-            <Route path="/search">
-              <SearchResultDisplay />
-            </Route>
-            <Route exact
-              path="/courseDetails/:code"
-              render={props =>(<CourseDescriptionPage {...props} />)}>
-            </Route>
-            <Route path="/">
-              <SearchResultDisplay />
-            </Route>
-
-          </Switch>
-        </div>
-        
-        
-    
-      </Router>
-    );
-  }
+							<Nav>
+								<Nav.Link as={Link} to='/lance_page'>
+									ECE444-Lab1
+								</Nav.Link>
+							</Nav>
+						</Navbar.Collapse>
+					</Navbar>
+				</div>
+				<div>
+					<Switch>
+						<Route path='/about'>
+							<div className='body_text'>
+								<p>
+									Welcome to CARTE's in-development tool for course selection at
+									UofT. Education Pathways allows for more intelligent course
+									searching, by matching not just the terms you search, but ones
+									relevant to them. The more terms you search for, the more
+									relevant your results will be! Even try searching across
+									disciplines for the courses that best cover each. Whatever
+									year you are looking for, Education Pathways will also suggest
+									courses in earlier years that will best help you to prepare.
+									To get the most out of this, try searching for courses in a
+									later year and see what is suggested for your current one. We
+									are looking for feedback to improve Education Pathways and
+									make it more useful for students. If you have ideas or
+									suggestions, please{' '}
+									<a href='mailto:alex.olson@utoronto.ca'> email us! </a>{' '}
+									<br></br>
+								</p>
+								<p>
+									<b>Development Team: </b>
+								</p>
+								<p>
+									Alexander Olson{' '}
+									<a href='https://carte.utoronto.ca/'> (CARTE)</a>{' '}
+								</p>
+								<p>
+									Student team from{' '}
+									<a href='https://shuiblue.github.io/UofT-ECE444/'>
+										ECE444-Fall2021
+									</a>{' '}
+									: Janelle Cuevas, Jean Lin, Terry Luan, Cansin Varol, Nick Woo
+								</p>
+							</div>
+							{/* <SearchResultDisplay /> */}
+						</Route>
+						<Route path='/lance_page'>
+							<p>Lance Canlas</p>
+						</Route>
+						<Route path='/search'>
+							<SearchResultDisplay />
+						</Route>
+						<Route
+							exact
+							path='/courseDetails/:code'
+							render={(props) => <CourseDescriptionPage {...props} />}
+						></Route>
+						<Route path='/'>
+							<SearchResultDisplay />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
 }
